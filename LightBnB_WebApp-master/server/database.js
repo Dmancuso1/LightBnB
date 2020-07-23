b250194439171714a22e530a76d6a8a949e2e5ca
Dane Mancuso <danegmancuso@gmail.com>
2020-07-23 19:26:05 +0000

    migrated getAllReservations to sql

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -61,6 +61,7 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
+
   const queryString = `INSERT INTO users(name,email,password) VALUES ($1, $2,$3) RETURNING *;`
   return pool.query(queryString, [user.name, user.email, user.password])
   .then(res => res.rows[0]);
@@ -74,8 +75,23 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = function(guest_id, limit) {
+  const queryStringResos = `
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `
+  return pool.query(queryStringResos, [guest_id, limit = 10])
+  .then(res => {
+    console.log(res.rows[0]); 
+    return res.rows;
+  });
 }
 exports.getAllReservations = getAllReservations;
 
